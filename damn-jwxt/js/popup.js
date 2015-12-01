af31537399ff66a2a11bd31f60a13c6d09e8a383
XBT1 <xbtop1@gmail.com>
2015-12-01 20:11:36 +0800

    updated

--- damn-jwxt/js/popup.js
+++ damn-jwxt/js/popup.js
@@ -1,22 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var rate = {}, comment = {}, req = {}, reqStr;
-	document.getElementById('aha').onclick = function () {
-		this.className = 'aha';
-		setTimeout(function () {
-			this.className = '';
-		}.bind(this), 5000);
-	};
+	var comments = [
+		'课程内容很充实，教师教学很风趣。',
+		'老师很好！教得贼棒了!',
+		'不错不错',
+		'好老师',
+		'真是一门好课啊！很喜欢呢~',
+		'希望加油！'
+	];
+	var eleComment1 = document.getElementsByName('comment-1')[0];
+	var eleComment2 = document.getElementsByName('comment-2')[0];
+	eleComment1.value = comments[parseInt(Math.random() * comments.length)];
+	eleComment2.value = comments[parseInt(Math.random() * comments.length)];
 	document.getElementById('go').onclick = function () {
 		rate[0] = document.getElementsByName('a')[0].value;
 		rate[1] = document.getElementsByName('b')[0].value;
 		rate[2] = document.getElementsByName('c')[0].value;
 		rate[3] = document.getElementsByName('d')[0].value;
-		comment[0] = document.getElementsByName('comment-1')[0].value;
-		comment[1] = document.getElementsByName('comment-2')[0].value;
+		comment[0] = eleComment1.value;
+		comment[1] = eleComment2.value;
 		req[0] = rate;
 		req[1] = comment;
 		reqStr = JSON.stringify(req);
-		console.log(reqStr);
 		chrome.tabs.executeScript(null, {
 			code: "fastChoose(" + reqStr + ");", allFrames: true
 		});
