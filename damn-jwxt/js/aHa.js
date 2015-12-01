de4af9c703935f661685d96e06ddef47f27f0e30
snokier <me@snokier.com>
2015-12-01 22:06:43 +0800

    updated

--- damn-jwxt/js/aHa.js
+++ damn-jwxt/js/aHa.js
@@ -1,47 +1,57 @@
 function fastChoose(data) {
-	var rate = data[0],
-		comment = data[1],
-		rateSum = 0,
-		fliter,
-		radio,
-		random,
-		button;
-	var fliter1 = /^[0-9.]{1,}_1$/,
-		fliter2 = /^[0-9.]{1,}_0.8$/,
-		fliter3 = /^[0-9.]{1,}_0.6$/,
-		fliter4 = /^[0-9.]{1,}_0.4$/;
+	var rate = data[0];
+	var	comments = data[1];
+
 	var mainFrame = window.frames['bottomFrame'].frames['mainFrame'];
 	var textArea = mainFrame.document.getElementsByTagName('textarea');
-	var radios = mainFrame.document.getElementsByTagName('table')[4].getElementsByTagName('table')[6].getElementsByTagName('table')[0].getElementsByTagName('input');
+	var radiosTable = mainFrame.document.getElementsByTagName('table')[4].getElementsByTagName('table')[6].getElementsByTagName('table')[0];
 	var buttons = mainFrame.document.getElementsByTagName('img');
-	for (var i = 0; i < 4; i++) {
+
+	for (var i = 0, rateSum = 0; i < 4; i++) {
 		rateSum += parseInt(rate[i]);
 	}
-	for (var i in radios) {
-		radio = radios[i];
-		random = Math.ceil(Math.random() * rateSum);
-		if (random <= parseInt(rate[0])) {
-			fliter = fliter1;
-		} else if (random <= parseInt(rate[0]) + parseInt(rate[1])) {
-			fliter = fliter2;
-		} else if (random <= parseInt(rate[0]) + parseInt(rate[1]) + parseInt(rate[2])) {
-			fliter = fliter3;
-		} else {
-			fliter = fliter4;
-		}
-		if (fliter.test(radio.value)) {
-			if(!radio.checked) {
-				radio.checked = true;
+
+	radioChoose(radiosTable, rateSum, rate); // 选择优良中差
+	textAreaFill(textArea, comments);	// 填写评价
+	formSubmit(buttons);	// 提交
+}
+
+function radioChoose(radiosTable, rateSum, rate) {
+	var trs = radiosTable.getElementsByTagName('tr');
+	var radios, random;
+
+	for(var i = 0; i< trs.length; i++) {
+		var index;
+		if(!trs[i].hasAttribute('align')) {
+			radios = trs[i].getElementsByTagName('input');
+			if(!radios.length) {
+				continue;
 			}
+			random = Math.ceil(Math.random() * rateSum);
+			if (random <= parseInt(rate[0])) {
+				index = 0;
+			} else if (random <= parseInt(rate[0]) + parseInt(rate[1])) {
+				index = 1;
+			} else if (random <= parseInt(rate[0]) + parseInt(rate[1]) + parseInt(rate[2])) {
+				index = 2;
+			} else {
+				index = 3;
+			}
+			radios[index].checked = true;
 		}
 	}
+}
+
+function textAreaFill(textArea, comments) {
 	for (var i = 0; i < 2; i++) {
-		textArea[i].value = comment[i];
+		textArea[i].value = comments[i];
 	}
+}
+
+function formSubmit(buttons) {
 	for (var i in buttons) {
-		button = buttons[i];
-		if (button.title == "提交") {
-			button.click();
+		if (buttons[i].title == "提交") {
+			buttons[i].click();
 		}
 	}
 }
